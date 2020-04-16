eecb98b95638bb5eeb6260d5a1a119f4a4c9c958
Alberto <alberto.blacutt@jalasoft.com>
2020-04-16 13:21:57 -0400

    Adding last changes

--- src/Contexts/Global/requests.js
+++ src/Contexts/Global/requests.js
@@ -4,6 +4,7 @@ export default function requests(state, dispatch, actions) {
 
   return {
     loadUsers: async function() {
+      // async call
       const users = await userRest.getUsers();
       dispatch(actions.UPDATE_STATE({ users }));
       return users;
